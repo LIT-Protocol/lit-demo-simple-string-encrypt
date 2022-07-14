e1041746562e4400de02ff20492aaf5b61f37c61
Anson <anson@Ansons-MacBook-Pro-2.local>
2022-07-14 17:04:04 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import * as LitJsSdk from 'lit-js-sdk';
+
+// -- init litNodeClient
+const litNodeClient = new LitJsSdk.LitNodeClient();
+litNodeClient.connect();
 
 function App() {
+
+  const go = async () => {
+
+    const messageToEncrypt = "this is a secret message";
+
+    const chain = 'ethereum';
+
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({chain})
+
+    const accessControlConditions = [
+      {
+        contractAddress: '',
+        standardContractType: '',
+        chain: 'ethereum',
+        method: 'eth_getBalance',
+        parameters: [':userAddress', 'latest'],
+        returnValueTest: {
+          comparator: '>=',
+          value: '0',  // 0 ETH, so anyone can open
+        },
+      },
+    ];
+
+    // 1. Encryption
+    // <Blob> encryptedString
+    // <Uint8Array(32)> symmetricKey 
+    const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(messageToEncrypt);
+
+    console.warn("symmetricKey:", symmetricKey);
+    
+    // 2. Saving the Encrypted Content to the Lit Nodes
+    // <Unit8Array> encryptedSymmetricKey
+    const encryptedSymmetricKey = await litNodeClient.saveEncryptionKey({
+      accessControlConditions,
+      symmetricKey,
+      authSig,
+      chain,
+    });
+    
+    console.warn("encryptedSymmetricKey:", encryptedSymmetricKey);
+    console.warn("encryptedString:", encryptedString);
+
+    // 3. Decrypt it
+    // <String> toDecrypt
+    const toDecrypt = LitJsSdk.uint8arrayToString(encryptedSymmetricKey, 'base16');
+    console.log("toDecrypt:", toDecrypt);
+
+    // <Uint8Array(32)> _symmetricKey 
+    const _symmetricKey = await litNodeClient.getEncryptionKey({
+      accessControlConditions,
+      toDecrypt,
+      chain,
+      authSig
+    })
+
+    console.warn("_symmetricKey:", _symmetricKey);
+
+    // <String> decryptedString
+    const decryptedString = await LitJsSdk.decryptString(
+      encryptedString,
+      symmetricKey
+    );
+
+    console.warn("decryptedString:", decryptedString);
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => go()}>GO</button>
       </header>
     </div>
   );
